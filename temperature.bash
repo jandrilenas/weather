3be5db1007312fa06f8fa2ad827f76a39860bebb
justinintime <jandrilenas@ufl.edu>
2016-09-23 11:19:31 -0400

    Add files via upload

---
+++ temperature.bash
@@ -0,0 +1,4 @@
+yesterday = `date -d yesterday +%Y/%m/%d`
+curl -s "https://www.wunderground.com/history/airport/GNV/${yesterday}/DailyHistory.heml?&format=1" > weather.txt
+max_temp = ` cat weather.txt | awk -F "," '{print $2}' | sort -n | tail -n1`
+echo the max temp is $max_temp
